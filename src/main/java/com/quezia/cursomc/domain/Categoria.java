4700ba3f95199959aa8b06b6d8ff4a51e3f560b8
kitten404 <quirinoquezia@gmail.com>
2020-03-28 13:40:17 -0300

    Primeira classe de dominio criada

---
+++ src/main/java/com/quezia/cursomc/domain/Categoria.java
@@ -0,0 +1,60 @@
+package com.quezia.cursomc.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer Id;
+	private String Nome;
+	
+	public Categoria() {
+		
+	}
+		
+	public Categoria(Integer id, String nome) {
+		super();
+		Id = id;
+		Nome = nome;
+	}
+
+	public Integer getId() {
+		return Id;
+	}
+	public void setId(Integer id) {
+		Id = id;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		return true;
+	}
+
+}
--- src/main/java/com/quezia/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/quezia/cursomc/resources/CategoriaResource.java
@@ -3,15 +3,26 @@ package com.quezia.cursomc.resources;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+
+import com.quezia.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
+	
+	public List<Categoria> listar() {
 		
-		return "Rest Está Funcionando";
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista= new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
