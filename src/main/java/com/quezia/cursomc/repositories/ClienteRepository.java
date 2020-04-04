6931d2da809eeb6f44a552ba801495785a9280e5
kitten404 <quirinoquezia@gmail.com>
2020-04-04 19:13:18 -0300

    validacao de email personalizada

--- src/main/java/com/quezia/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/quezia/cursomc/repositories/ClienteRepository.java
@@ -2,12 +2,14 @@ package com.quezia.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.quezia.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente,Integer> {
 	
-	
+	@Transactional
+	Cliente findByEmail(String email);
 
 }
--- src/main/java/com/quezia/cursomc/services/validator/ClienteInsertValidator.java
+++ src/main/java/com/quezia/cursomc/services/validator/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.quezia.cursomc.domain.Cliente;
 import com.quezia.cursomc.domain.enums.TipoCliente;
 import com.quezia.cursomc.dto.ClienteNewDTO;
+import com.quezia.cursomc.repositories.ClienteRepository;
 import com.quezia.cursomc.resources.FieldMessage;
 
  
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> { 
  
+	@Autowired
+	private ClienteRepository ci;
+	
     @Override 
     public void initialize(ClienteInsert ann) {    
     	
@@ -31,6 +38,14 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
         if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj()) ) {
         	
         	list.add(new FieldMessage("cpfOuCnpj","CNPJ Inválido"));
+        	
+        }
+        
+        Cliente cl = ci.findByEmail(objDto.getEmail());
+        
+        if(cl!= null) {
+        	
+        	list.add(new FieldMessage("email","Email Já Existente"));
         	
         }
         
