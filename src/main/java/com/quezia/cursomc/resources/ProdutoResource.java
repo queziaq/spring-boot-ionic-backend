c275f709f43e67b60691021c84d4bb420678ecd9
kitten404 <quirinoquezia@gmail.com>
2020-04-05 13:28:40 -0300

    Procurando produto por categoria e nome

--- src/main/java/com/quezia/cursomc/resources/ProdutoResource.java
+++ src/main/java/com/quezia/cursomc/resources/ProdutoResource.java
@@ -1,14 +1,21 @@
 package com.quezia.cursomc.resources;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import com.quezia.cursomc.domain.Produto;
+import com.quezia.cursomc.dto.ProdutoDTO;
+import com.quezia.cursomc.resources.utils.URL;
 import com.quezia.cursomc.services.ProdutoService;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -26,6 +33,21 @@ public class ProdutoResource {
 		Produto prod = serv.buscar(id);
 		
 		return ResponseEntity.ok().body(prod);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<Page<ProdutoDTO>> FindPage(
+			@RequestParam(value="nome" ,defaultValue="") String nome,
+			@RequestParam(value="categorias" ,defaultValue="") String categorias, 
+			@RequestParam(value="page" ,defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage" ,defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue ="nome")String orderBy, 
+			@RequestParam(value="direction" ,defaultValue="ASC")String direction) {
+		String nomeDecoder = URL.decodeParam(nome);
+		List<Integer> ids = URL.decodeIntList(categorias);
+		Page<Produto> list = serv.search(nomeDecoder, ids, page, linesPerPage, orderBy, direction);
+		Page<ProdutoDTO> cDTO = list.map(obj -> new ProdutoDTO(obj));
+		return ResponseEntity.ok().body(cDTO);
 	}
 
 }
--- src/main/java/com/quezia/cursomc/services/ProdutoService.java
+++ src/main/java/com/quezia/cursomc/services/ProdutoService.java
@@ -1,11 +1,17 @@
 package com.quezia.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.quezia.cursomc.domain.Categoria;
 import com.quezia.cursomc.domain.Produto;
+import com.quezia.cursomc.repositories.CategoriaRepository;
 import com.quezia.cursomc.repositories.ProdutoRepository;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -16,11 +22,21 @@ public class ProdutoService {
 	@Autowired
 	private ProdutoRepository cr;
 	
+	@Autowired
+	private CategoriaRepository categoriaRepo;
+	
 	public Produto buscar(Integer id) throws ObjectNotFoundException {
 		Optional<Produto> obj = cr.findById(id);
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " 
 		+ id + ", Tipo: " + Produto.class.getName()));
 	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias =  categoriaRepo.findAllById(ids);
+		return cr.findDistinctByNomeContainingAndCategoriasIn(nome, categorias,pageRequest);
+	}
+	
 	
 }
