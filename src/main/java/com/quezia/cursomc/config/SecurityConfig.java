35b9474c1fd865a296938230705113d3727ac073
kitten404 <quirinoquezia@gmail.com>
2020-04-10 21:42:42 -0300

    implementando autorizacao

--- src/main/java/com/quezia/cursomc/config/SecurityConfig.java
+++ src/main/java/com/quezia/cursomc/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.quezia.cursomc.security.JWTAuthenticationFilter;
+import com.quezia.cursomc.security.JWTAuthorizationFilter;
 import com.quezia.cursomc.security.JWTUtil;
 
 @Configuration
@@ -52,6 +53,7 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 			.antMatchers(PUBLIC_MATCHERS)
 			.permitAll().anyRequest().authenticated();
 			http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+			http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil,userDetailsService));
 			http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		}
 		@Override
