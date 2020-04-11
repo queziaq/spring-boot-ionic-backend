139df485e4c0184360815aa752eb6b610533f894
kitten404 <quirinoquezia@gmail.com>
2020-04-11 13:47:05 -0300

    refresh token

--- src/main/java/com/quezia/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/quezia/cursomc/repositories/PedidoRepository.java
@@ -1,13 +1,19 @@
 package com.quezia.cursomc.repositories;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.quezia.cursomc.domain.Cliente;
 import com.quezia.cursomc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido,Integer> {
 	
-	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 
 }
--- src/main/java/com/quezia/cursomc/services/PedidoService.java
+++ src/main/java/com/quezia/cursomc/services/PedidoService.java
@@ -4,9 +4,11 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.quezia.cursomc.domain.Cliente;
 import com.quezia.cursomc.domain.ItemPedido;
 import com.quezia.cursomc.domain.PagamentoComBoleto;
@@ -18,6 +20,7 @@ import com.quezia.cursomc.repositories.ItemPedidoRepository;
 import com.quezia.cursomc.repositories.PagamentoRepository;
 import com.quezia.cursomc.repositories.PedidoRepository;
 import com.quezia.cursomc.repositories.ProdutoRepository;
+import com.quezia.cursomc.security.UserSS;
 
 import javassist.tools.rmi.ObjectNotFoundException;
 
@@ -79,4 +82,15 @@ public class PedidoService {
 		return obj;
 	}
 	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSS user = UserService.authenticated();
+		if(user == null)
+			throw new AuthorizationExcpetion("Acesso Negado");
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Optional<Cliente> op =  cr.findById(user.getId());
+		Cliente cli = op.get();
+		return pr.findByCliente(cli, pageRequest);
+
+	}
 }
